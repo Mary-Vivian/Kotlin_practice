3b17d0bc8a5b7df39d4a4d4ca6532ce87be595d2
Mary-Vivian <muthonimaryvivian>
2024-03-16 21:11:23 +0300

    kotlin practice

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,182 @@
+////fun main() {
+////
+////}
+////
+////fun modulus(){
+////   var num1=30
+////   var num2=38
+////   var result=num1%num2
+////   println(result)
+////}
+////fun countchar(){
+////   var word="Avocado"
+////   var wordlen=word.length
+////   println()
+////}
+////fun printmyname(name:String){
+//////   println(name)
+//////}
+////fun main(){
+////    var names= arrayOf("Jane", "Anne", "Mary", "Asha")
+////    println(names[0])
+//////    println(names[0])
+//////}
+////fun main(){
+////    multiply(6,7)
+////    multiply(8,9)
+////    printName("Leilah")
+////    printName("Skyler")
+////    subtract(78,56)
+////    var result=subtract(678,89)
+////    getFullName("Caren","Chepngetich")
+////
+////}
+////fun multiply(num1:Int, num2: Int){
+////    var product=num1*num2
+////}
+////fun printName(name:String){
+////    println("Hello my name is "+ name)
+////}
+////fun subtract(num3:Int,num4:Int):Int{
+////    var difference=num3-num4
+////    println(difference)
+////    return difference
+////
+////}
+////fun getFullName(firstName:String, lastName:String):String{
+////    var fullName=firstName+ " " + lastName
+////    return fullName
+////    println(fullName)
+////}
+////fun main(){
+////    printString("akirachix")
+////
+////}
+////fun printString(name:String) {
+////    println(name)
+//////    println(name.first())
+//////    println(name[3])
+//////    println(name[4])
+////    var conc = name[0] + ""  + name[3]+ ""+ name[4]
+//////    println(conc)
+//////}
+////fun main(){
+////    printstring("worldhey")
+////
+////}
+////fun printstring(word:String){
+////    var a=word[0]
+////    var b=word[3]
+////    var c=word[4]
+//////    var add=a + "" +b+"" +c
+////    var add="$a$b$c"
+////    println(add)
+//////
+//////}
+////fun main(){
+////    myname("chichewa")
+////    checkName("jane")
+////    checkName("name")
+////    areaValue(8)
+////
+////}
+////    fun myname(name:String) {
+////        var namelength = name.length
+////        println(namelength)
+////    }
+//////if else statement
+////fun checkName(name2: String) {
+////    if (name2 == "mary") {
+////        println("that's me")
+////    } else {
+////        println("I don't know who that is")
+////    }
+////}
+////    fun areaValue(radius:Int){
+////        var areamult=radius*radius*3.142
+////        println(areamult)
+////
+////    }
+////creating an app that captures student records
+////////*****************************************************
+////fun main(){
+////  myName("mary")
+////  myCitizen(false)
+////  myAge(23)
+//////   myWeight(78.9)
+////   myPhone("0789665")
+////
+////}
+////fun myName(name:String){
+////  println(name)
+////}
+////fun myAge(age:Int){
+////    println(age)
+////}
+////fun myPhone(phonenumber:String){
+////   println(phonenumber)
+////}
+////
+////fun myWeight(weight:Double){
+////   println(weight)
+////   }
+////fun myCitizen(citizenship:Boolean) {
+////val iskenyan="Kenyan"
+////   println(iskenyan)
+////
+////}
+//
+////fun main(){
+////    isEven(7)
+////}
+////fun isEven(num:Int):Boolean{
+////    if (num%2==0){
+////        println("Even")
+////        return true
+////    }
+////    else{
+////        println("odd")
+////        return false
+////    }
+////}
+//fun main(){
+//    gap(23)
+//    daysOfweek(12)
+//    daysOfweek(4)
+//
+//}
+//fun gap(age:Int){
+//    if (age < 18){
+//        println("Child")
+//    }
+//    else{
+//        println("Adult")
+//    }
+//
+//}
+//fun daysOfweek(day:Int){
+//   val result= when (day){
+//        1->"Monday"
+//        2->"Tuesday"
+//        3->"Wednesday"
+//        4->"Thursday"
+//        5->"Friday"
+//        6->"Saturday"
+//        7->"Sunday"
+//        else ->"Invalid day"
+//    }
+//    println(result)
+//}
+//
+fun main(){
+
+}
+
+
+
+
+
+
+
+
+
